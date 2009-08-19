1d83efeb4ef636ecfc319059cc8519ee82e95ba0
Leon Timmermans <fawaka@gmail.com>
2009-08-19 14:58:21 +0200

    Refactored resource management

--- src/resources.c
+++ src/resources.c
@@ -12,18 +12,43 @@
  * !!! global data !!!
  */
 
-perl_mutex thread_lock;
 bool inited = 0;
-UV current = 0;
-UV allocated = 0;
-mthread** threads = NULL;
+
+typedef struct {
+	perl_mutex lock;
+	UV current;
+	UV allocated;
+	void** objects;
+	size_t size;
+} resource;
+
+S_resource_init(resource* res, UV preallocate, size_t size) {
+	MUTEX_INIT(&res->lock);
+	res->size = size;
+	res->objects = safemalloc(preallocate * size);
+	res->allocated = preallocate;
+}
+
+#define resource_init(res, pre, type) S_resource_init(res, pre, sizeof(type))
+
+void resource_addobject(resource* res, void* object) {
+	if (res->current == res->allocated)
+		res->objects = saferealloc(res->objects, res->size * (res->allocated *=2));
+	res->objects[res->current++] = object;
+}
+
+void resource_removeobject(resource* res, UV id) {
+	MUTEX_LOCK(&res->lock);
+	res->objects[id] = NULL;
+	MUTEX_UNLOCK(&res->lock);
+}
+
+resource threads;
 
 void global_init(pTHX) {
 	if (!inited) {
-		MUTEX_INIT(&thread_lock);
 		inited = TRUE;
-		Newxz(threads, 8, mthread*);
-		allocated = 8;
+		resource_init(&threads, 8, mthread*);
 		mthread* ret = mthread_alloc();
 #  ifdef WIN32
 		ret->thr = GetCurrentThreadId();
@@ -43,26 +68,22 @@ mthread* mthread_alloc(IV linked_to) {
 		ret->listeners.list[0] = linked_to;
 		ret->listeners.alloc = ret->listeners.head = 1;
 	}
-	MUTEX_LOCK(&thread_lock);
-	ret->id = current;
-	if (current == allocated)
-		Renew(threads, allocated *=2, mthread*);
-	threads[current++] = ret;
-	MUTEX_UNLOCK(&thread_lock);
+	MUTEX_LOCK(&threads.lock);
+	ret->id = threads.current;
+	resource_addobject(&threads, ret);
+	MUTEX_UNLOCK(&threads.lock);
 	return ret;
 }
 
 void mthread_destroy(mthread* thread) {
-	MUTEX_LOCK(&thread_lock);
-	threads[thread->id] = NULL;
-	MUTEX_UNLOCK(&thread_lock);
+	resource_removeobject(&threads, thread->id);
 	queue_destroy(&thread->queue);
 }
 
 static mthread* S_get_thread(pTHX_ UV thread_id) {
-	if (thread_id >= current || threads[thread_id] == NULL)
+	if (thread_id >= threads.current || threads.objects[thread_id] == NULL)
 		Perl_croak(aTHX_  "Thread %"UVuf" doesn't exist", thread_id);
-	return threads[thread_id];
+	return threads.objects[thread_id];
 }
 
 #define get_thread(id) S_get_thread(aTHX_ id)
@@ -83,11 +104,11 @@ static mthread* S_get_thread(pTHX_ UV thread_id) {
 void S_thread_send(pTHX_ UV thread_id, message* message) {
 	dXCPT;
 
-	MUTEX_LOCK(&thread_lock);
+	MUTEX_LOCK(&threads.lock);
 	THREAD_TRY {
 		mthread* thread = get_thread(thread_id);
-		queue_enqueue(&thread->queue, message, &thread_lock);
-	} THREAD_FINALLY( MUTEX_UNLOCK(&thread_lock) );
+		queue_enqueue(&thread->queue, message, &threads.lock);
+	} THREAD_FINALLY( MUTEX_UNLOCK(&threads.lock) );
 }
 
 void S_send_listeners(pTHX_ mthread* thread, message* message) {
@@ -96,11 +117,11 @@ void S_send_listeners(pTHX_ mthread* thread, message* message) {
 	MUTEX_LOCK(&thread->lock);
 	int i;
 	for (i = 0; i < thread->listeners.head; ++i) {
-		MUTEX_LOCK(&thread_lock);
+		MUTEX_LOCK(&threads.lock);
 		UV thread_id = thread->listeners.list[i];
-		if (thread_id >= current || threads[thread_id] == NULL)
+		if (thread_id >= threads.current || threads.objects[thread_id] == NULL)
 			continue;
-		queue_enqueue_copy(&threads[thread_id]->queue, message, &thread_lock);
+		queue_enqueue_copy(&((mthread*)threads.objects[thread_id])->queue, message, &threads.lock);
 	}
 	MUTEX_UNLOCK(&thread->lock);
 }
@@ -110,7 +131,7 @@ void S_send_listeners(pTHX_ mthread* thread, message* message) {
 void S_thread_add_listener(pTHX_ UV talker, UV listener) {
 	dXCPT;
 
-	MUTEX_LOCK(&thread_lock);
+	MUTEX_LOCK(&threads.lock);
 	THREAD_TRY {
 		mthread* thread = get_thread(talker);
 		if (thread->listeners.alloc == thread->listeners.head) {
@@ -118,6 +139,6 @@ void S_thread_add_listener(pTHX_ UV talker, UV listener) {
 			Renew(thread->listeners.list, thread->listeners.alloc, IV);
 		}
 		thread->listeners.list[thread->listeners.head++] = listener;
-	} THREAD_FINALLY( MUTEX_UNLOCK(&thread_lock) );
+	} THREAD_FINALLY( MUTEX_UNLOCK(&threads.lock) );
 }
 
