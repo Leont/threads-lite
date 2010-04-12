f893118499b058959013071c7d28366e0932c99b
Leon Timmermans <fawaka@gmail.com>
2010-04-12 12:49:16 +0300

    Added atomic wait counters

--- src/sync.c
+++ src/sync.c
@@ -5,6 +5,9 @@
 #define atomic_sub(address, value) __sync_sub_and_fetch(address, value)
 #define atomic_replace(address, old_value, new_value) __sync_bool_compare_and_swap(address, old_value, new_value)
 
+void spin_init(spin_lock_t* lock) {
+	lock->count = 0;
+}
 void spin_lock(spin_lock_t* lock) {
 	do {
 		while (lock->count);
@@ -15,6 +18,9 @@ void spin_unlock(spin_lock_t* lock) {
 	atomic_replace(&lock->count, 1, 0);
 }
 
+void lock_init(shared_lock_t* lock) {
+	lock->count = 0;
+}
 void lock_shared(shared_lock_t* lock) {
 	int value;
 	redo:
@@ -42,4 +48,23 @@ void lock_exclusive(shared_lock_t* lock) {
 void unlock_exclusive(shared_lock_t* lock) {
 	if (!atomic_replace(&lock->count, -1, 0))
 		abort();
+}
+
+void counter_init(atomic_counter* counter, int value) {
+	counter->counter = value;
+	sem_init(&counter->waiter, 0, 0);
+}
+void counter_inc(atomic_counter* counter) {
+	atomic_add(&counter->counter, 1);
+}
+void counter_dec(atomic_counter* counter) {
+	if (atomic_sub(&counter->counter, 1) == 0)
+		sem_post(&counter->waiter);
+}
+void counter_wait(atomic_counter* counter) {
+	if (atomic_sub(&counter, 1) != 0)
+		sem_wait(&counter->waiter);
+}
+void counter_destroy(atomic_counter* counter) {
+	sem_destroy(&counter->waiter);
 }
--- src/sync.h
+++ src/sync.h
@@ -1,7 +1,10 @@
+#include <semaphore.h>
+
 typedef struct {
 	int count;
 } spin_lock_t;
 
+void spin_init(spin_lock_t* lock);
 void spin_lock(spin_lock_t* lock);
 void spin_unlock(spin_lock_t* lock);
 
@@ -9,7 +12,19 @@ typedef struct {
 	int count;
 } shared_lock_t;
 
+void lock_init(shared_lock_t* lock);
 void lock_shared(shared_lock_t* lock);
 void unlock_shared(shared_lock_t* lock);
 void lock_exclusive(shared_lock_t* lock);
 void unlock_exclusive(shared_lock_t* lock);
+
+typedef struct {
+	int counter;
+	sem_t waiter;
+} atomic_counter;
+
+void counter_init(atomic_counter*, int);
+void counter_inc(atomic_counter*);
+void counter_dec(atomic_counter*);
+void counter_wait(atomic_counter*);
+void counter_destroy(atomic_counter*);
