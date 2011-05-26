23095cf00b6ea3c84075b08043c2145a191f66f5
Leon Timmermans <fawaka@gmail.com>
2011-05-27 00:14:32 +0200

    Revert "Removed mXPUSHs usage for 5.10.0 compatibity"
    This reverts commit 3ecee6022e9813070b0be7c5f6c203846a0f3d51.
    
    Conflicts:
    
    	src/mthread.c

--- src/mthread.c
+++ src/mthread.c
@@ -169,10 +169,10 @@ static void* run_thread(void* arg) {
 		call = SvRV(message_load_value(to_run));
 
 		PUSHMARK(SP);
-		XPUSHs(sv_2mortal(newSVpvn("exit", 4)));
-		status = sv_2mortal(newSVpvn("normal", 6));
-		XPUSHs(status);
-		XPUSHs(sv_2mortal(newSViv(thread->id)));
+		mXPUSHs(newSVpvn("exit", 4));
+		status = newSVpvn("normal", 6);
+		mXPUSHs(status);
+		mXPUSHs(newSViv(thread->id));
 
 		ENTER;
 		PUSHMARK(SP);
